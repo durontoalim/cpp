964f3e3cbe9d25ec3b11643448aca08de2135dc3
Utin <durontoalim711@gmail.com>
2022-01-27 23:30:04 +0600

    First

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+int main()
+{
+
+    int a = 20;
+    float b = 20.43;
+
+    double c = 30.345;
+
+    char d = 'A' ;
+
+    string strng = " hewewgwegew " ;
+
+    cout << a << " ";
+    cout << b << " ";
+    cout << c << " ";
+
+    cout << d << " ";
+    cout << strng;
+
+    cout << (a> 15 && b<25);
+
+    //String Length
+
+
+    string str_length = "Hello" ;
+
+    cout << str_length.length() << " ";
+
+    cout << str_length.size() ;
+
+
+
+    return 0;
+}
